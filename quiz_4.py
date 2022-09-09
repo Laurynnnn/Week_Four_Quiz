9af6dbec04480aa413cc582432e875c60f95d0a9
Lauryn Laeticia Kantono <87691653+Laurynnnn@users.noreply.github.com>
2022-09-09 19:23:26 +0300

    Update quiz_4.py

--- quiz_4.py
+++ quiz_4.py
@@ -48,3 +48,43 @@ def tmarks(list):
         print("Your grade is A")
 
 tmarks(marks)
+
+#6. Write a function for checking the speed of drivers. This function should have one parameter: speed.
+# If speed is less than 70, it should print “Ok”.
+# Otherwise, for every 5km above the speed limit (70), it should give the driver 
+# one demerit point and print the total number of demerit points. For example, 
+# if the speed is 80, it should print: “Points: 2”.
+# If the driver gets more than 12 points, the function should print: “License suspended”
+count = 0
+def speed_check(speed):
+    if speed < 70:
+        print("Ok")
+    elif speed > 70:
+        while(True):
+            speed - 5
+            points = count + 1
+        print("Demerit Points: {points}")
+        if points > 12:
+            print("Licence suspended")
+            
+
+speed = int(input("Please enter speed of driver: "))
+speed_check(speed)
+
+    
+
+
+#7.  Write a function called show_stars(rows). If rows is 5, it should print the following:
+# *
+# **
+# ***
+# ****
+# *****
+def show_rows(rows):
+    for i in range(rows):
+        for k in range(rows):
+            print("*", end ="")
+        
+    print()
+    
+show_rows(5)
